ae6636b4cde9ba0fcc1872407fe0721758a7a710
WindomZ <git.windomz@gmail.com>
2017-04-12 21:32:13 +0800

    test: add fmtconv testings

---
+++ test/fmtconv.test.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Windom on 2017/4/12.
+ */
+const test = require('ava')
+
+const fmtconv = require('../lib/fmtconv')
+
+function fmtconvPromise (input, output) {
+  return new Promise(resolve => {
+    fmtconv(input, output)
+    resolve()
+  })
+}
+
+test('fmtconv fail', async (t) => {
+  await fmtconvPromise()
+    .then(() => {
+      t.fail('should be catch error')
+    })
+    .catch(() => t.pass())
+
+  await fmtconvPromise('./examples/yaml-1.yaml')
+    .then(() => {
+      t.fail('should be catch error')
+    })
+    .catch(() => t.pass())
+
+  await fmtconvPromise('./examples/yaml-1.xxx',
+    './examples/yaml-1.test.fmtconv.yaml')
+    .then(() => {
+      t.fail('should be catch error')
+    })
+    .catch(() => t.pass())
+
+  await fmtconvPromise('./examples/yaml-1.yaml',
+    './examples/yaml-1.test.fmtconv.xxx')
+    .then(() => {
+      t.fail('should be catch error')
+    })
+    .catch(() => t.pass())
+})
+
+test('fmtconv example-1 pass', async (t) => {
+  await fmtconvPromise('./examples/yaml-1.yaml',
+    './examples/yaml-1.fmtconv.yaml')
+    .then(() => {
+      t.pass()
+    })
+    .catch(err => t.fail(err))
+
+  await fmtconvPromise('./examples/yaml-1.yaml',
+    './examples/yaml-1.fmtconv.json')
+    .then(() => {
+      t.pass()
+    })
+    .catch(err => t.fail(err))
+
+  await fmtconvPromise('./examples/json-1.json',
+    './examples/json-1.fmtconv.json')
+    .then(() => {
+      t.pass()
+    })
+    .catch(err => t.fail(err))
+
+  await fmtconvPromise('./examples/json-1.json',
+    './examples/json-1.fmtconv.yaml')
+    .then(() => {
+      t.pass()
+    })
+    .catch(err => t.fail(err))
+})
